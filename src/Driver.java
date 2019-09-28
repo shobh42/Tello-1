288740effe86f1174bfea38ce28d74ee269e0929
Shobhit <smundra42@gmail.com>
2019-09-28 16:32:07 -0500

    Added switch statement for different command. Strategy pattern can be used

--- src/Driver.java
+++ src/Driver.java
@@ -41,15 +41,86 @@ public class Driver {
         }
 
 
-        if(isConnected){
+        while(isConnected){
             System.out.println("Enter the message");
             String message = sc.nextLine();
+
+            switch (message){
+                case "command":
+                    flier.executeCommand(message);
+                    break;
+
+                case "takeoff":
+                    flier.executeCommand(message);
+                    break;
+
+                case "land":
+                    flier.executeCommand(message);
+                    break;
+
+                case "up":
+                    flier.executeCommand(message);
+                    break;
+
+                case "left":
+                    flier.executeCommand(message);
+                    break;
+
+                case "right":
+                    flier.executeCommand(message);
+                    break;
+
+                case "forward":
+                    flier.executeCommand(message);
+                    break;
+
+                case "back":
+                    flier.executeCommand(message);
+                    break;
+
+                case "cw":
+                    flier.executeCommand(message);
+                    break;
+
+                case "cww":
+                    flier.executeCommand(message);
+                    break;
+
+                case "flip":
+                    flier.executeCommand(message);
+                    break;
+
+                case "go":
+                    flier.executeCommand(message);
+                    break;
+
+                case "stop":
+                    flier.executeCommand(message);
+                    break;
+
+                case "speed":
+                    flier.executeCommand(message);
+                    break;
+
+                case "battery":
+                    flier.executeCommand(message);
+                    break;
+
+                case "time":
+                    flier.executeCommand(message);
+                    break;
+
+                default:
+                    System.out.println("Unknown Message");
+
+            }
+
             try{
                 flier.executeCommand(message);
             }catch (Exception e){
+                isConnected = false;
                 System.out.println(e.getMessage());
             }
-
         }
     }
 }
--- src/DroneNotConnectedException.java
+++ src/DroneNotConnectedException.java
@@ -3,6 +3,6 @@ import java.net.InetAddress;
 public class DroneNotConnectedException extends Exception {
 
     public DroneNotConnectedException(InetAddress ip, int port){
-        super("There is connection issue with the drone at: " + ip.toString() + ":" + port );
+        super("There is an error connecting to drone at: " + ip.toString() + ":" + port );
     }
 }
